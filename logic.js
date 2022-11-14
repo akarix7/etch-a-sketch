b5dc27fbfecd6d0d387efb2d5a191809dcd5022a
akarix7 <116602617+akarix7@users.noreply.github.com>
2022-11-13 23:59:40 -0600

    add dynamic createGrid and hovering effect

--- logic.js
+++ logic.js
@@ -1,9 +1,35 @@
+const container = document.querySelector("#container");
+
 function createGrid(gridSize){
-    const container = document.querySelector("#container");
     for(let i = 0; i < gridSize; i++){
-        let square = document.createElement("div");
-        container.appendChild(square);
+        for(let j = 0; j < gridSize; j++) {
+            createDiv(Math.floor((750 - gridSize*2)/gridSize));
+        }
     }
+    fillBox();
+}
+
+function createDiv(size){
+    let square = document.createElement("div");
+    square.className = "box";
+    square.style.width = `${size}px`;
+    square.style.height = `${size}px`;
+    container.appendChild(square);
+}
+
+function fillBox(){
+    const cell = document.querySelectorAll(".box");
+    console.log("hey");
+
+    cell.forEach((c) => { c.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "#33CC00";
+    }, false)
+    });
+
+    cell.forEach((c) => {c.addEventListener("mouseout", (e) => {
+        e.target.style.backgroundColor = "#33CC00";
+    }, false)
+    });
 }
 
 createGrid(16);
